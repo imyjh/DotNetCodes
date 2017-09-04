c6de4bdc039b6874691fb4d7537e45eb0414300e
奎宇工作室 <79599287@qq.com>
2017-09-04 16:09:10 +0800

    更新 TimeHelper.cs

--- DotNet.Utilities/时间戳/TimeHelper.cs
+++ DotNet.Utilities/时间戳/TimeHelper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+/*
+ 源码己托管:http://git.oschina.net/kuiyu/dotnetcodes
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,5 +33,45 @@ namespace DotNet.Utilities.时间戳
             System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
             return Convert.ToString((int)(time - startTime).TotalSeconds);
         }
+
+        /// <summary>
+		/// 获取以0点0分0秒开始的日期
+		/// </summary>
+		/// <param name="d"></param>
+		/// <returns></returns>
+		public static DateTime GetStartDateTime(DateTime d)
+		{
+			if (d.Hour != 0)
+			{
+				var year = d.Year;
+				var month = d.Month;
+				var day = d.Day;
+				var hour = "0";
+				var minute = "0";
+				var second = "0";
+				d = Convert.ToDateTime(string.Format("{0}-{1}-{2} {3}:{4}:{5}", year, month, day, hour, minute, second));
+			}
+			return d;
+		}
+
+		/// <summary>
+		/// 获取以23点59分59秒结束的日期
+		/// </summary>
+		/// <param name="d"></param>
+		/// <returns></returns>
+		public static DateTime GetEndDateTime(DateTime d)
+		{
+			if (d.Hour != 23)
+			{
+				var year = d.Year;
+				var month = d.Month;
+				var day = d.Day;
+				var hour = "23";
+				var minute = "59";
+				var second = "59";
+				d = Convert.ToDateTime(string.Format("{0}-{1}-{2} {3}:{4}:{5}", year, month, day, hour, minute, second));
+			}
+			return d;
+		}
     }
 }
