c6827068fd8c485fa4030bdd0480d5203caa54e3
Tony <79599287@qq.com>
2017-12-20 10:59:23 +0800

    add webapp client

---
+++ SocketServer/WebAppClient/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WebSocket4Net;
+
+namespace WebAppClient
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        WebSocket websocket;
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            //打开socket 
+
+            string url = "ws://127.0.0.1:2017";
+           
+            websocket = new WebSocket(url);
+            websocket.Opened += websocket_Opened;
+            websocket.Closed += websocket_Closed;
+            websocket.MessageReceived += websocket_MessageReceived;
+            websocket.Open();
+            SocketClient.websocket = websocket;
+        }
+
+        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+
+        }
+
+        private void websocket_Closed(object sender, EventArgs e)
+        {
+            SocketClient.websocket.Open();
+        }
+
+        private void websocket_Opened(object sender, EventArgs e)
+        {
+            SocketClient.websocket.Send("asp.net online");
+        }
+    }
+}
---
+++ SocketServer/WebAppClient/SocketClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSocket4Net;
+
+namespace WebAppClient
+{
+    public static class SocketClient
+    {
+
+        public static WebSocket websocket { get; set; }
+
+        
+    }
+}
