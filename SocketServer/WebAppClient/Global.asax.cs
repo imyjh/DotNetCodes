d38c5c3eda1948e3e952abaea207bf4e64633be1
Tony <79599287@qq.com>
2018-01-13 13:24:04 +0800

    fix socket client error

--- SocketServer/WebAppClient/Global.asax.cs
+++ SocketServer/WebAppClient/Global.asax.cs
@@ -11,7 +11,7 @@ namespace WebAppClient
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        WebSocket websocket;
+        
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -20,31 +20,9 @@ namespace WebAppClient
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
 
-            //打开socket 
-
-            string url = "ws://127.0.0.1:2017";
-           
-            websocket = new WebSocket(url);
-            websocket.Opened += websocket_Opened;
-            websocket.Closed += websocket_Closed;
-            websocket.MessageReceived += websocket_MessageReceived;
-            websocket.Open();
-            SocketClient.websocket = websocket;
+         
         }
 
-        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
-        {
-
-        }
-
-        private void websocket_Closed(object sender, EventArgs e)
-        {
-            SocketClient.websocket.Open();
-        }
-
-        private void websocket_Opened(object sender, EventArgs e)
-        {
-            SocketClient.websocket.Send("asp.net online");
-        }
+       
     }
 }
--- SocketServer/WebAppClient/SocketClient.cs
+++ SocketServer/WebAppClient/SocketClient.cs
@@ -6,11 +6,39 @@ using WebSocket4Net;
 
 namespace WebAppClient
 {
-    public static class SocketClient
+    public  class SocketClient
     {
 
-        public static WebSocket websocket { get; set; }
+        public WebSocket websocket { get; set; }
 
-        
+
+        public SocketClient()
+        {
+            string url = "127.0.0.1:2017";
+            //string authKey = AuthUrl.CreateAuthKey(url);
+            // url = url + "/" + authKey;
+            websocket = new WebSocket(url);
+            websocket.Opened += websocket_Opened;
+            websocket.Closed += websocket_Closed;
+            websocket.MessageReceived += websocket_MessageReceived;
+            websocket.Open();
+        }
+
+        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+
+        }
+
+        private void websocket_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void websocket_Opened(object sender, EventArgs e)
+        {
+
+        }
+ 
+
     }
 }
